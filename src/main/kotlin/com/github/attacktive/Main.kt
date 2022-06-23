9ee29083c31f558e71d2bbfc9ef0628db16d0e55
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-06-23 17:30:05 +0900

    Implement Reading Real .rpgsave File (#6)
    * Implement reading real .rpgsave file
    
    The file is taken from https://f95zone.to/threads/divine-miko-koyori-final-circle-poison-kagura-games.44859/page-15
    
    * Fix default file name

---
+++ src/main/kotlin/com/github/attacktive/Main.kt
@@ -0,0 +1,16 @@
+package com.github.attacktive
+
+import com.github.attacktive.file.FileUtils
+import com.github.diogoduailibe.lzstring4j.LZString
+import java.nio.charset.StandardCharsets
+
+fun main(vararg args: String) {
+	val defaultFileName = "file1.rpgsave"
+	val argument = args.getOrNull(0)
+	val fileName = if (argument.isNullOrBlank()) { defaultFileName } else { argument }
+
+	val bytes = FileUtils.readFileFromResources("/${fileName}")
+	val base64 = String(bytes, StandardCharsets.UTF_8)
+	val decompressed = LZString.decompressFromBase64(base64)
+	println(decompressed)
+}
